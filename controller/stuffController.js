160cdd197a0fbb986c2eebed252834e132a80ba5
Grondin Kevin <kevin.grdn37800@gmail.com>
2022-03-30 19:28:09 +0200

    Fin

--- controller/stuffController.js
+++ controller/stuffController.js
@@ -1,8 +1,8 @@
 exports.getEveryone = (req, res, next) => {
     let dbPath = require('../data.json');
-    let data = [data.push(dbPath)]
+    
 
-    res.status(200).json({data});
+    res.status(200).json({dbPath});
 
 }
 
@@ -14,10 +14,6 @@ exports.getRegion = (req, res, next) => {
     let data = []
     let offset = parseInt(req.params.offset) 
      
-
-    
-
-
     var total = 0;
 
     for(let i = 0; i < dbPath.length; i++){
@@ -32,7 +28,15 @@ exports.getRegion = (req, res, next) => {
      
    
 
-
     res.status(200).json({data, offset, total});
 
+}
+
+exports.getNumber = (req, res, next) => {
+    let numberUrl = req.params.number
+    var phoneNum = /0[6|7|2]\d{8}/; 
+    
+        res.status(200).json(phoneNum.test(numberUrl))
+
+    
 }
--- routes/stuff.js
+++ routes/stuff.js
@@ -6,5 +6,6 @@ const controller = require('../controller/stuffController')
 
 router.get('/', controller.getEveryone)
 router.get('/:region/:offset', controller.getRegion )
+router.get('/:number', controller.getNumber)
 
 module.exports = router;
